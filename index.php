bb68fa4462ffbbba032d14298f562ce5f1d99fb0
Seakmeng Chheang <c.seakmeng0603@gmail.com>
2020-01-30 23:24:09 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php
+        define('TITLE', 'HOME');
+        include('template/header.php');
+    ?>
+</head>
+<body>
+    <?php include('template/navbar.html') ?>
+
+    <?php include 'php/home/sec_1.php'; ?>
+    <?php include 'php/home/sec_2.php'; ?>
+    <?php include 'php/home/sec_3.php'; ?>
+    <?php include 'php/home/sec_4.php'; ?>
+
+    <?php include ('template/footer.html') ?>
+</body>
+</html>
+
