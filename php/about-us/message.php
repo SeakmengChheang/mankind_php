5c5ad9a51e4616ec907fcb6c6db5f0e48ec956e0
Visal Som <visalsom3616@gmail.com>
2020-02-05 16:47:19 +0700

    Footer and About-Us

---
+++ php/about-us/message.php
@@ -0,0 +1,31 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img class="card-img-top" src="../../img/Medical Equipment/10.jpg" alt="Card image cap">  
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <br>
+            <br>
+            <p>        
+                As a medical doctor and Hospital Director, I’ve done quite a good number of health checks for various reasons since I started practicing medicine in 1981. Sometimes it was pre-insurance checkup, a lot of times it was a regular annual checkup as one of the corporate citizen and a few times pre-employment checkup before start a job at new organization. My pre-employment health checked was done the last time in June 2017 before I started my current job as Hospital Director of the Mankind Medicare. 
+            </p>
+            <p>
+                But having my heart checked the first time with CCTA (Coronary Computed Tomography Angiography) came with specific special reason. It was January 2005 when the new 64-Slice State of the Art CT just arrived at the Bangkok Hospital Pattaya, Pattaya City, Thailand, and the Radiology Department needed 10 volunteers to test run the machine and the CCTA procedure. CCTA is a heart imaging test that helps determine if plaque buildup has narrowed the coronary arteries, the blood vessels that supply the heart. Plaque is made up of various substances such as fat, cholesterol and calcium that deposit along the inner lining of the arteries. Plaque which builds up overtime can reduce or in some cases completely block blood flow. Patients undergoing a CCTA scan receive an iodine-containing contrast material as an intravenous injection to ensure the best possible images of the heart blood vessels.  At that time my Calcium Score was Zero which is good and there was no significant stenosis of the coronary artery system for me. I was fully awake during the procedure with no medication and did not feel pain or any unpleasant feeling except warm, flushed sensation running through my body for around half a minute from contrast media circulating in my body. Of the 10 volunteers, 2-3 of them need further investigation and cardiac procedure to dilate the cardiac vessels. Like my father, who I had him checked with  CCTA just a few months after me, I’ve a dominant right coronary artery. My father developed Acute Myocardial Infarction five years ago at the distal part right coronary artery, despite normal CCTA.
+            </p>
+            <p>
+                I’ve got my 2<sup>nd</sup> CCTA done in December 2018 at the Royal Phnom Penh Hospital after I come back from my medical class alumni meeting. It was approximately 13 years since the first procedure. Time flied, we’ve passed our prime health and some of my classmates already got coronary artery dilatation, stenting and Cardiac Bypass Surgery. A few of them died or battling some form of cancer. My maternal grandfather and my younger brother died from lung cancer and I want to have my lung checked simultaneously with the CCTA. I also have a personality type A, slightly overweight and have a genotypic predictive value of high risk for cardiac coronary artery disease.
+            </p>
+            <p>
+                I am glad that, after the 2<sup>nd</sup> CCTA, my heart is still going strong with healthy blood vessels supply my cardiac muscle. I have Zero calcium score and no cardiac vessels stenosis. This is one of the best New Year gift for me, my family and The Royal Phnom Penh Hospital. I am full of joy and energy and would love to remind you of my story and your health.
+            </p>
+            <p>
+                <strong>Happy New Year 2020 From My Heart</strong>
+            </p>
+            <p>
+                <strong>Alexandar The Great, Grand Master Of Doctor</strong>
+            </p>
+        </div>
+    </div>
+</div>
---
+++ php/about-us/vision.php
@@ -0,0 +1,50 @@
+<div class="container">
+  <div class="row">
+    <div class="col-md-8">
+        <h2 class="text-primary">Our Mission</h2>
+        <p>We intend to be a leader in providing medical services that are recognized internationally by using modern technology and by efficiently administering our professional network.</p>
+        <p>Our medical teams adhere to the highest standards and professional ethics, and are committed to continuously improve their services to the benefit and satisfaction of our patients.</p>
+    </div>
+    <div class="col-md-4">
+        <h2 class="text-primary">Our Vision</h2>
+        <p>Mankind Medicare is place that provides excellent care of the highest standards of international quality to promote trust and satisfaction to Cambodians, expatriates and tourists.</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <h2 class="text-primary">Quality Policy</h2>
+      <p>Mankind Medicare shall be the medical service facilities with high standard and international quality, for trustworthiness and optimal satisfaction of the customers. Royal Phnom Penh Hospital intends to continually improve the management patient safety system and its services by emphasizing on the continuous quality improvement throughout the organization.</p>
+      <p>We</p>
+      <p>
+          <ul>
+              <li>Achieve patient safety through professional risk management throughout our hospital</li>
+              <li>Use medication safely</li>
+              <li>Provide highest levels of infection control</li>
+              <li>Provide a safe environment</li>
+          </ul>
+      </p>
+    </div>
+  </div>
+  
+  <div class="row">
+      <div class="col">
+          <h2 class="text-primary">Safety Policy</h2>
+          <p>Mankind Medicare operates its business with responsibility to its patients, the hospital personnel, all concerned persons and to society by paying great attention to preserve the environment, occupational health and safety as well as by continually improving the system.</p>
+      </div>
+  </div>
+  
+  <div class="row">
+      <div class="col">
+          <h2 class="text-primary">The Second JCI acrredited Hospital in Cambodia</h2>
+          <p>With dedication to excellence, Mankind Medicare has been accredited by the Joint Commission International (JCI) in recognition of their international medical service and healthcare practices. We have consistently developed our medical expertise to achieve the highest possible standards because when it comes to trust, only the best will do.</p>
+          <p>
+              <ul>
+                  <li>Truly International</li>
+                  <li>Qualified</li>
+                  <li>Trust Expertise</li>
+                  <li>Modern Technology</li>
+              </ul>
+          </p>
+      </div>
+  </div>
+</div>
