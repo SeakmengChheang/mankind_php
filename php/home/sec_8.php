d3ddb6abb4527d21fbec8da968269025b48162d2
Seakmeng Chheang <c.seakmeng0603@gmail.com>
2020-02-29 14:53:11 +0700

    add search button

--- php/home/sec_8.php
+++ php/home/sec_8.php
@@ -6,7 +6,10 @@
             <div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3908.5734929007876!2d104.89579291533525!3d11.582402691777833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3109517bf7757d23%3A0x965c34888684bf1!2sParagon%20International%20University!5e0!3m2!1sen!2skh!4v1581061409343!5m2!1sen!2skh" 
                     width="100%" 
-                    height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                    height="100%" 
+                    frameborder="0"
+                    style="border:0;" 
+                    allowfullscreen=""></iframe>
             </div>
         </div>
 
